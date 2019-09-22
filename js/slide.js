884acbf2b9ec9ef7044cd07f3ccee192e953801d
HYEOK999 <iasg2004@naver.com>
2019-09-23 01:17:30 +0900

    release : Add the Javacript #3
    1. Add the Carousel function to Javacript (but, fail...)

---
+++ js/slide.js
@@ -0,0 +1,20 @@
+// function slideShow(){
+    
+//     var index = 0;
+//     var img = new Array(document.getElementById("img-1"),document.getElementById("img-2"),document.getElementById("img-3"));
+
+//     var intervalId = setTimeout( function(){
+//         if(img[index].checked == true){
+//             img[index].checked = false;    
+//         }
+//         else{
+//             index ++;
+//             img[index].checked = true;
+//         }
+//         if(index < img.length){
+//             index = 0;
+//         }
+//     },2000);
+// }
+
+// slideShow();
