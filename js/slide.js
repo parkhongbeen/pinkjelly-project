e8eb643ab92c0093743d83bfbe9d16e134fd9f1d
eunbiviakwon <eunbiviakwon@gmail.com>
2019-09-23 15:59:35 +0900

    헤더 부분 만들기

--- js/slide.js
+++ js/slide.js
@@ -1,20 +1,22 @@
-// function slideShow(){
-    
-//     var index = 0;
-//     var img = new Array(document.getElementById("img-1"),document.getElementById("img-2"),document.getElementById("img-3"));
+function slideShow(){
+    var index = 0;
 
-//     var intervalId = setTimeout( function(){
-//         if(img[index].checked == true){
-//             img[index].checked = false;    
-//         }
-//         else{
-//             index ++;
-//             img[index].checked = true;
-//         }
-//         if(index < img.length){
-//             index = 0;
-//         }
-//     },2000);
-// }
+    // console.log(document.getElementById("img-1"))
+    var img = document.querySelectorAll('#img-1, #img-2, #img-3, #img-4, #img-5, #img-6, #img-7');
 
-// slideShow();
+    var intervalId = setInterval( function(){
+        if(img[index].checked == true){
+            img[index].checked = false;    
+        }
+        if (img[index].checked == false){
+            index ++;
+            img[index].checked = true;
+        }
+        if(index == 6){
+            console.log(123)
+            index = 0;
+        }
+    },2000);
+}
+
+slideShow();
